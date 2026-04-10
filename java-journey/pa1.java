b9c916f5c9bc4805c002b9e4e9b197933607a193
adityameshram-dev <adityabmeshram358@gmail.com>
2026-04-10 07:30:15 +0530

    Add files via upload

---
+++ java-journey/pa1.java
@@ -0,0 +1,11 @@
+import mypackage.p1;
+
+public class pa1 {
+    public static void main(String[] args) {
+        p1 p = new p1();
+        System.out.println(p.add(5,5));
+        System.out.println(p.sub(3, 1));
+    }
+}
+
+// here i import that package
